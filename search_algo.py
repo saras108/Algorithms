64bf3491f0f296aba9abd4876ab48680592a2d6e
Saras <saras.me108@gmail.com>
2020-10-16 12:55:39 +0545

    iteration ans recurtion completed of bineary and linear search and started the jump search

--- search_algo.py
+++ search_algo.py
@@ -1,38 +1,4 @@
-def linear_search(list , num):
-    #the time complexity of this algo is O(n) as in the worst case it have to go through all the list.
-
-    for i in range(len(list)):
-        if(list[i] == num ):
-            return i
-    return "The data which you have searched is not found in the Array."
-    
-
-
-
-def binary_search(list , num , starting , ending):
-    #the time complexity of this algo is O(log base 2 n) as
-    #1st iteration: n/2 part on list is seprated
-    #2nd iteration: n/4 part is seprated
-    #3rd iteration: n/8 part of list is seperated
-    # and in the worst case : n/n part of to list is seprated, so its time complexity is O(log base 2 n)
-
-
-    if(starting > ending):
-        #if ending index is greater then the starting index, the alogo have been through all the list and it havent found on the list. 
-        return "The data which you have entered does not lies on the list."
-
-    mid = (ending+starting)//2
-
-    print(list[starting : ending])
-
-    if list[mid] == num:
-        return mid
-    elif list[mid] > num:
-        return binary_search(list ,num ,starting , mid-1)
-    else:
-        return binary_search(list, num , mid+1 , ending )
-
-
+#ask the input array from user
 def get_array():
     my_arr = []
 
@@ -41,23 +7,129 @@ def get_array():
     for i in range(arr_num):
         a = int(input("enter the number"))
         my_arr.append(a)
-    
+
     return my_arr
-        
 
 list = get_array()
 
 
-# get_index = linear_search(list , int(input("the number to be found")))
+
+
+# linear Search
+    # the time complexity of this algo is O(n) as in the worst case it have to go through all the list.
+
+def linear_search_iteration(list, num):
+
+    for i in range(len(list)):
+        if(list[i] == num):
+            return i
+
+    return "The data which you have searched is not found in the Array."
+
+
+def linear_search_recursion(list, num , starting , ending):
+
+    # print(list[starting: ending])
+
+    if starting >ending:
+        return "The data which you have searched is not found in the Array."
+    
+    if list[starting] == num:
+        return starting
+    
+    else:
+        return linear_search_recursion(list , num , starting+1 , ending)
+
+
+# get_index = linear_search_iteration(list , int(input("the number to be found")))
 # print(get_index)
 
-get_index = binary_search(list , int(input("Number you want to search")) , 0 , len(list))
-print(get_index)
+# get_index = linear_search_recursion(list , int(input("the number to be found")) , 0, len(list)-1)
+# print(get_index)
 
 
 
-# A = [2,6, 9,10 , 11 ,12, 15 ,18 ]
-# num = 12
-# a = binary_search(A , num , 0 ,len(A))
+
+
+#Binary Search
+    # the time complexity of this algo is O(log base 2 n) as
+    # 1st iteration: n/2 part on list is seprated
+    # 2nd iteration: n/4 part is seprated
+    # 3rd iteration: n/8 part of list is seperated
+    # and in the worst case : n/n part of to list is seprated, so its time complexity is O(log base 2 n)
+
+
+def binary_search_recursion(list, num, starting, ending):
+
+    if(starting > ending):
+        # if starting index is greater then the ending index, the alogo have been through all the list and it havent found on the list.
+        return "The data which you have entered does not lies on the list."
+
+    mid = (ending+starting)//2
+
+    # print(list[starting: ending])
+
+    if list[mid] == num:
+        return mid
+
+    elif list[mid] > num:
+        return binary_search_recursion(list, num, starting, mid-1)
+
+    else:
+        return binary_search_recursion(list, num, mid+1, ending)
+
+
+def binary_search_iteration(list , num):
+
+    mid = 0
+    starting = 0
+    ending = len(list)-1
+
+    while(starting <= ending):
+        mid = (starting+ending)//2
+
+        if list[mid]== num:
+            return mid
+        
+        elif list[mid] < num:
+            starting = mid+1
+
+        else:
+            ending = mid-1
+
+    return "The data which you are searching is not available in this list."
+
+
+
+
+# get_index = binary_search_recursion(list, int(input("Number you want to search")), 0, len(list)-1)
+# print(get_index)
+
+# get_index = binary_search_iteration(list, int(input("Number you want to search")))
+# print(get_index)
+
+
+
+
+def jum_search_iteration():
+    pass
+
+def jump_search_recurtion():
+    pass
+
+
+
+
+
+
+
+
+
+A = [2,6, 9,10 , 11 ,12, 15 ,18 ]
+num = 30
+# a = binary_search_iteration(A , num)
+# a = binary_search_recursion(A, num, 0, len(A)-1)
 
 # print(a)
+
+
