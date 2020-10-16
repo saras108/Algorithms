b49bde1b88fe6ae5b522fdc0f38a48277eb6bbc4
Saras <saras.me108@gmail.com>
2020-10-16 23:40:50 +0545

    basic seaching algorithms

--- search_algo.py
+++ search_algo.py
@@ -13,7 +13,7 @@ def get_array():
 
     return my_arr
 
-list = get_array()
+# list = get_array()
 
 
 #Binary Search
@@ -30,7 +30,7 @@ def binary_search_recursion(list, num, starting, ending):
         # if starting index is greater then the ending index, the alogo have been through all the list and it havent found on the list.
         return "The data which you have entered does not lies on the list."
 
-    mid = (ending+starting)//2
+    mid = int((ending+starting)//2)
 
     # print(list[starting: ending])
 
@@ -148,11 +148,6 @@ def jump_search_iteration(list , num):
     return linear_search_recursion(list , num , prev , id)
 
 
-
-# get_index = jump_search_iteration(list , int(input("the number to be found")))
-# print(get_index)
-
-
 def jump_search_recursion(list , num , step , prev , id):
     if list[id] <= num:
         if list[id]== num:
@@ -173,18 +168,118 @@ def jump_search_recursion(list , num , step , prev , id):
         return linear_search_recursion(list , num , prev , id)
 
 
+# get_index = jump_search_iteration(list , int(input("the number to be found")))
+# print(get_index)
+
 # get_index = jump_search_recursion(list , int(input("the number to be found")) ,  int(math.sqrt(len(list))), 0 , int(math.sqrt(len(list))))
 # print(get_index)
 
 
 
 
+
+
+#Interpolation Search
+
+# Interpolation search is the searching techinique using which we can get the searched index in :
+# O(log(logn)) time complecity which is better then binery search, 
+# and in the worst case we can get our value in O(n) which is similer to the linear search.
+
+def interpolation_search_iteration(list , num):
+    start = 0
+    end = len(list)-1
+    check_point = int(start + (end - start)/(list[end]- list[start]) * (num - list[start]))
+
+    if(list[start] > num or list[end] < num):
+        return "Number out of range."
+
+
+    while start <= end :
+        if list[check_point] == num:
+            return check_point
+        elif list[check_point] > num:
+            end = check_point -1
+        else:
+            start = check_point + 1
+
+    return "The number which you have searched is not found."
+
+def interpolation_search_recursion(list , num , start , end , check_point):
+    
+    if(list[start] > num or list[end] < num):
+        return "Number out of range."
+
+    check_point = int(start + (end - start)/(list[end]- list[start]) * (num - list[start]))
+
+    if(start > end):
+        return "Number doesnt belong to the list provided."
+    else:
+        if list[check_point] == num:
+            return check_point
+        elif list[check_point] > num:
+            return interpolation_search_recursion(list , num , start , check_point - 1 , check_point)
+        else:
+            return interpolation_search_recursion(list , num , check_point+1 , end , check_point)
+    
+
+# get_index = interpolation_search_iteration(list , int(input("the number to be found")))
+# print(get_index)
+
+# num = int(input("the number to be found"))
+# get_index = interpolation_search_recursion(list , num , 0 , len(list)-1 , int(0 + (len(list)-1 - 0)/(list[len(list)-1]- list[0]) * (num - list[0])))
+# print(get_index)
+
+
+
+
+#Exponantional Search
+#this shearch has search complexity O(logN) which is similer to that of binary search
+#1st iterration => 2^1
+#2nd iterration => 2^2
+#3rd iterration => 2^3
+#nth iterration => 2^n
+def exponantional_search(list , num):
+    i = 1
+    if list[0]==num:
+        return '0'
+    else:
+        while i<len(list) and list[i] <= num:
+            i = i* 2
+        return binary_search_recursion(list, num , i/2 , min(i , len(list)-1))
+    
+# get_index = interpolation_search_iteration(get_array() , int(input("the number to be found")))
+# print(get_index)
+
+
+
+#Ternary Search
+#this search is similer to binary search having 2 mid-points.
+
+def ternary_search(list , num , start , end):
+    if(start > end):
+        return "The number is not in this list"
+    else:
+        mid_1 = start + (end -start) // 3
+        mid_2 = end - (end -start) // 3
+
+        if(list[mid_1]==num):
+            return mid_1
+        if(list[mid_2] == num):
+            return mid_2
+        
+        if(num < list[mid_1]):
+            return ternary_search(list , num , start , mid_1-1)
+
+        elif(num >  list[mid_2]):
+            return ternary_search(list , num , mid_2+1 , end)
+
+        else:
+            return ternary_search(list , num , mid_1+1 , mid_2-1)
+
+
+
 A = [2,6, 9,10 , 11 ,12, 15 ,18, 20, 21 , 22 ,25 ]
-num = 1
-# b = jump_search_iteration(A , num)
-# a = jump_search_recursion(A, num, int(math.sqrt(len(A))), 0 , int(math.sqrt(len(A))))
+num = 10
+a = ternary_search(A, num, 0, len(A)-1 )
 
-# print(a)
-
-
-# print(int(math.sqrt(5)))
+print(a)
